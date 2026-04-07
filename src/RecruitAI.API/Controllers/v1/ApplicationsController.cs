53f9aa26743f552c813db46d4f9adf6e008927d3
Quang Duy <87593754+quaqduy@users.noreply.github.com>
2026-04-07 18:39:12 +0700

    hotfix

--- src/RecruitAI.API/Controllers/v1/ApplicationsController.cs
+++ src/RecruitAI.API/Controllers/v1/ApplicationsController.cs
@@ -5,7 +5,7 @@ using RecruitAI.Application.Commands.Jobs;
 using RecruitAI.Application.Commands.Applications;
 using RecruitAI.Application.DTOs.Common;
 using RecruitAI.Application.DTOs.Requests.Applications;
-using RecruitAI.Application.DTOs.Requests.Jobs;
+using RecruitAI.Application.DTOs.Requests.Applications;
 using RecruitAI.Application.DTOs.Responses.Applications;
 using RecruitAI.Application.Interfaces;
 using RecruitAI.Application.Interfaces.Services;
