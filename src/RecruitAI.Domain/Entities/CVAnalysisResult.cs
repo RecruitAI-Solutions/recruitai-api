b7f8629749c48298cd38ba958d55b6a4e61085ff
Quang Duy <quaqduy.it@gmail.com>
2026-04-23 01:47:57 +0700

    fix CV analys

--- src/RecruitAI.Domain/Entities/CVAnalysisResult.cs
+++ src/RecruitAI.Domain/Entities/CVAnalysisResult.cs
@@ -3,6 +3,8 @@ namespace RecruitAI.Domain.Entities
 {
 	public class CVAnalysisResult
 	{
+		private double _confidence;
+
 		public Guid Id { get; set; }
 		public Guid CVId { get; set; }
 		public int SkillId { get; set; }
@@ -12,5 +14,11 @@ namespace RecruitAI.Domain.Entities
 		// Navigation properties
 		public virtual CV CV { get; set; } = null!;
 		public virtual Skill Skill { get; set; } = null!;
+		public double Confidence
+		{
+			get => _confidence;
+			set => _confidence = Math.Round(value, 2); // làm tròn 2 chữ số thập phân
+		}
+
 	}
 }
