a1fcc27a2660a589764b4bc2f67f474bbdb9a839
Quang Duy <quaqduy.it@gmail.com>
2026-04-23 02:23:14 +0700

    add pagination for jobs suggest CV

--- src/RecruitAI.Application/Queries/Jobs/GetJobSuggestionsByCVQuery.cs
+++ src/RecruitAI.Application/Queries/Jobs/GetJobSuggestionsByCVQuery.cs
@@ -1,12 +1,15 @@
 ﻿// RecruitAI.Application/Queries/Jobs/GetJobSuggestionsByCVQuery.cs
 using MediatR;
+using RecruitAI.Application.DTOs.Common;
 using RecruitAI.Shared.DTOs;
 
 namespace RecruitAI.Application.Queries.Jobs
 {
-	public class GetJobSuggestionsByCVQuery : IRequest<List<JobMatchResultDto>>
+	public class GetJobSuggestionsByCVQuery : IRequest<PaginationResponseDto<JobMatchResultDto>>
 	{
 		public Guid CVId { get; set; }
 		public int MinMatchSkills { get; set; } = 2;
+		public int Page { get; set; } = 1;
+		public int PageSize { get; set; } = 10;
 	}
 }
