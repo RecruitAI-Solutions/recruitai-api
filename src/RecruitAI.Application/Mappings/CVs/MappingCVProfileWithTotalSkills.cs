62b4139714cdc988ab1bd56376f398dc11d341c0
Quang Duy <quaqduy.it@gmail.com>
2026-04-22 20:03:27 +0700

    fix: add AutoMapper mapping for CVList to CVListDto with TotalSkills
    - Thêm mapping configuration cho TotalSkills trong AutoMapper profile
    - Thêm property TotalSkills vào CVListDto
    - Fix lỗi 500 khi gọi API GET /api/v1/CV/my-cvs

---
+++ src/RecruitAI.Application/Mappings/CVs/MappingCVProfileWithTotalSkills.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using RecruitAI.Application.DTOs.CVs;
+using RecruitAI.Domain.Common.CVs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecruitAI.Application.Mappings.CVs
+{
+	internal class MappingCVProfileWithTotalSkills : Profile
+	{
+		public MappingCVProfileWithTotalSkills()
+		{
+			// Mapping cho CVList -> CVListDto
+			CreateMap<CVList, CVListDto>()
+				.ForMember(dest => dest.TotalSkills,
+					opt => opt.MapFrom(src => src.TotalSkills))
+				.ForMember(dest => dest.FormattedFileSize,
+					opt => opt.MapFrom(src => FormatFileSize(src.FileSize)));
+		}
+
+		private string FormatFileSize(long bytes)
+		{
+			string[] sizes = { "B", "KB", "MB", "GB" };
+			double len = bytes;
+			int order = 0;
+			while (len >= 1024 && order < sizes.Length - 1)
+			{
+				order++;
+				len = len / 1024;
+			}
+			return $"{len:0.##} {sizes[order]}";
+		}
+
+	}
+}
