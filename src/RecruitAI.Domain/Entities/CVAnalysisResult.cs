8c9c63fbd80e8417866990155d552f370caea99f
Quang Duy <quaqduy.it@gmail.com>
2026-04-23 09:30:52 +0700

    Revert "hotfix deploy (#142)"
    This reverts commit 2be642145eca0db72a95cc12623f5db17a1db9e1.

--- src/RecruitAI.Domain/Entities/CVAnalysisResult.cs
+++ src/RecruitAI.Domain/Entities/CVAnalysisResult.cs
@@ -3,18 +3,17 @@ namespace RecruitAI.Domain.Entities
 {
 	public class CVAnalysisResult
 	{
+		private double _confidence;
 
 		public Guid Id { get; set; }
 		public Guid CVId { get; set; }
 		public int SkillId { get; set; }
+		public double Confidence { get; set; }
 		public DateTime CreatedAt { get; set; }
 
 		// Navigation properties
 		public virtual CV CV { get; set; } = null!;
 		public virtual Skill Skill { get; set; } = null!;
-
-		private double _confidence;
-
 		public double Confidence
 		{
 			get => _confidence;
