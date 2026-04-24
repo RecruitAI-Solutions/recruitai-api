b41da770e2753ffe7c6beb97c20a0fb5e05ac595
Quang Duy <quaqduy.it@gmail.com>
2026-04-24 18:04:24 +0700

    feat: add candidate dashboard API with job stats, applications, suggestions and reviews

---
+++ src/RecruitAI.Application/Queries/Candidate/GetCandidateDashboardQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using RecruitAI.Shared.DTOs;
+
+namespace RecruitAI.Application.Queries.Candidate
+{
+	// GetCandidateDashboardQuery.cs
+	public class GetCandidateDashboardQuery : IRequest<CandidateDashboardDto>
+	{
+		public Guid UserId { get; set; }
+	}
+}
